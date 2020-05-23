e95e98225b3a09ebf38337efc8ee1d083cc1bdec
abel <abelcheruvathoor@gmail.com>
2020-05-24 00:45:25 +0530

    UI update bug fixes,Added tabbed screens

---
+++ lib/screens/seeds_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SeedsTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Center(
+        child: Text(
+          "Seeds Tab",
+          style: TextStyle(color: Colors.black87),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/utilities_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UtilitiesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Center(
+        child: Text(
+          "Utilities Tab",
+          style: TextStyle(color: Colors.black87),
+        ),
+      ),
+    );
+  }
+}

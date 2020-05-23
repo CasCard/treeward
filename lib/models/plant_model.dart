391fd4e904c49e64262f3e121d7e010b16c75c17
abel <abelcheruvathoor@gmail.com>
2020-05-23 21:45:49 +0530

    Added new plant data ,code refactoring

--- lib/models/plant_model.dart
+++ lib/models/plant_model.dart
@@ -27,20 +27,39 @@ final List<Plant> plants=[
   ),
   Plant(
     imageUrl: 'assets/images/plant1.png',
-    name: 'Cool Plant',
+    name: 'Grandiflora',
     category: 'Indoor',
     price: 30,
     size: 'Medium',
     description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta risus id urna luctus efficitur.',
+        'It requires almost no attention and spreads itself very easily. In places with old architecture it can grow between the stones of the road or sidewalk.',
   ),
   Plant(
     imageUrl: 'assets/images/plant2.png',
-    name: 'Really Cool Plant',
+    name: 'Areca Palm',
     category: 'New Arrival',
     price: 42,
     size: 'Large',
     description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta risus id urna luctus efficitur. Suspendisse vulputate faucibus est, a vehicula sem eleifend quis.',
+    'Areca palm is also known as golden palm, butterfly palm, and yellow palm and is native to south India and the Philippines. Areca palm can be used as an indoor house plant and required special attention.',
   ),
+  Plant(
+    imageUrl: 'assets/images/plant3.png',
+    name: 'Snake Plant',
+    category: 'New Arrival',
+    price: 42,
+    size: 'Large',
+    description:
+    'It’s easy to care for, and will grow nicely in a sunny window or a spot that only has indirect light. You don’t even have to be all that regular watering it as it will do fine in dry soil.\n In fact, over-watering is one of the big problems with snake plants. Snake plants are an excellent choice for any room in the house and will help remove formaldehyde from your air.',
+  ),
+  Plant(
+    imageUrl: 'assets/images/plant4.png',
+    name: 'Tulsi',
+    category: 'New Arrival',
+    price: 42,
+    size: 'Large',
+    description:
+    'Mostly known as Holy Basil. It is native to India and mostly cultivated in south-eastern Asia. It is commonly used for treating Asthma, cold, sore throat, high bp and cholesterol.Also used as a stress relaxant and inflammation controller.',
+  ),
+
 ];
